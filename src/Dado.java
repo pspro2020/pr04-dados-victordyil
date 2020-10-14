024aef727782aae46c2150a6c9f20bb225596ce5
Victor Daniel Gil Becerra <victoryil27oficial@gmail.com>
2020-10-14 17:08:15 +0200

    Finish

---
+++ src/Dado.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Dado implements Runnable {
+    Resultados resultados;
+    Random random = new Random();
+
+    public Dado(Resultados resultados) {
+        this.resultados = resultados;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            resultados.incrementarPuntuacion(random.nextInt(6));
+        }
+    }
+
+}
